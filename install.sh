4b6a1972d207aff0fbb00a2fa0c9cc5417921269
fariz <mohamedfariz100@gmail.com>
2022-07-12 16:55:01 +0530

    Delete temporary files after installation

--- install.sh
+++ install.sh
@@ -4,5 +4,7 @@ python3 -m pip install -r requirements.txt;
 python3 -m isort __main__.py;
 python3 -m black __main__.py;
 pyinstaller __main__.py -n="TailorMate" -Dy;
+rm -r -v build
+rm -f -v TailorMate.spec
 ./dist/TailorMate/TailorMate;
 exit;
